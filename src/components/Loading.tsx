28c5d958f87efece59420b54bca37857349552d8
Edqe_ <34704796+Edqe14@users.noreply.github.com>
2022-11-25 14:11:47 +0700

    feat: init

--- src/components/Loading.tsx
+++
@@ -1,7 +0,0 @@
-import { LoadingOverlay, LoadingOverlayProps } from '@mantine/core';
-
-export default function Loading(props: LoadingOverlayProps) {
-  return (
-    <LoadingOverlay transitionDuration={200} loaderProps={{ color: 'orange' }} {...props} />
-  );
-}
